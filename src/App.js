12766f28d164b0798a2dff753184b3e92b309121
allwinp <59523783+allwinp@users.noreply.github.com>
2021-05-09 11:05:02 -0400

    cleaned up app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ import Services from './components/Services';
 import ContactForm from './components/ContactForm';
 import FluidAnimation from 'react-fluid-animation';
 import defaultConfig from './components/FluidAnimationdefaultConfig';
-import Typing from 'react-typing-animation';
 import FluidAnimationStrings from './components/FluidAnimationTextStrings'
 import LePetitPrinceBoyAndFox from './components/LePetitPrinceAndFox';
 import PrinceMoon from './components/Littleprince';
@@ -45,43 +44,7 @@ export default class App extends Component {
           config={config}
           animationRef={this._animationRef}
         />
-
-        <div className="FluidAnimationText"
-          style={{
-            position: 'absolute',
-            zIndex: 1,
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            padding: '1em',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            color: '#3832e2',
-            fontFamily: 'Verdana, "Helvetica Neue", sans-serif',
-            pointerEvents: 'none'
-          }}
-        >
-          <h1
-            style={{
-              fontSize: '5em',
-              textShadow: '2px 2px 8px rgba(0, 0, 0, 0.5)',
-            }}
-          >
-            <Typing speed={10} loop="true">
-              <span>{FluidAnimationStrings[0]}</span>
-              <Typing.Backspace delay={1000} count={FluidAnimationStrings[0].length + 1} />
-              <span>{FluidAnimationStrings[1]}</span>
-              <Typing.Backspace delay={1000} count={FluidAnimationStrings[1].length + 1} />
-              <span>{FluidAnimationStrings[2]}</span>
-              <Typing.Backspace delay={1000} count={FluidAnimationStrings[2].length + 1} />
-              <span>{FluidAnimationStrings[3]}</span>
-              <Typing.Backspace delay={1000} count={FluidAnimationStrings[3].length + 1} />
-            </Typing>
-          </h1>
-        </div>
+        <FluidAnimationStrings />
         </Route>
         <Route exact path="/about">
           <Homer />
@@ -101,22 +64,6 @@ export default class App extends Component {
       </div>
       </Switch>
     )
-  }
-
-  _animationRef = (ref) => {
-    this._animation = ref
-  }
-
-  _onUpdate = (config) => {
-    this.setState({ config })
-  }
-
-  _onClickRandomSplats = () => {
-    this._animation.addSplats(5 + Math.random() * 20 | 0)
-  }
-
-  _onReset = () => {
-    this.setState({ config: { ...defaultConfig } })
   }
 }
 
--- src/components/FluidAnimationTextStrings.js
+++ src/components/FluidAnimationTextStrings.js
@@ -1,3 +1,46 @@
+import React from 'react';
+import Typing from 'react-typing-animation';
+
 let strings = ['Allwin Philip', 'React & Django', 'Beautifully Styled', 'Responsive UI']
 
-export default strings
+const FluidAnimationStrings = () => {
+    return (
+        <div className="FluidAnimationText"
+        style={{
+          position: 'absolute',
+          zIndex: 1,
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          padding: '1em',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: '#3832e2',
+          fontFamily: 'Verdana, "Helvetica Neue", sans-serif',
+          pointerEvents: 'none'
+        }}
+        >
+        <h1
+          style={{
+            fontSize: '5em',
+            textShadow: '2px 2px 8px rgba(0, 0, 0, 0.5)',
+          }}
+        >
+          <Typing speed={10} loop="true">
+            <span>{strings[0]}</span>
+            <Typing.Backspace delay={1000} count={strings[0].length + 1} />
+            <span>{strings[1]}</span>
+            <Typing.Backspace delay={1000} count={strings[1].length + 1} />
+            <span>{strings[2]}</span>
+            <Typing.Backspace delay={1000} count={strings[2].length + 1} />
+            <span>{strings[3]}</span>
+            <Typing.Backspace delay={1000} count={strings[3].length + 1} />
+          </Typing>
+        </h1>
+        </div>    )
+}
+
+export default FluidAnimationStrings
